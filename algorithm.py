18cc0b8d610786c1ddba8336fbbb62622828db3c
Avdbreet <avanderbreetstraeten@gmail.com>
2020-03-06 19:17:18 +0100

    Add of example of keras constructin model. Importation of keras and tensorflow libraries. NEXT STEP: image deep learning datacamp

--- algorithm.py
+++ algorithm.py
@@ -15,7 +15,6 @@ Won't work, we're not into image analysis! But nice exercice, have to split the 
 Algorithm: KNeigborsClassifier
 
 """
-from sklearn.neighbors import KNeighborsClassifier
 import matplotlib.pyplot as plt
 import glob, pylab, pandas as pd
 import pydicom, numpy as np
@@ -23,6 +22,11 @@ import seaborn as sns
 from sklearn.model_selection import train_test_split
 
 
+#################################################################################
+################# KNeighbors classifier ########################################
+#################################################################################
+
+"""
 train_labels=pd.read_csv('/Users/Arthur/Desktop/ULB/2019-2020/Projet_Imagerie/rsna-pneumonia-detection-challenge/stage_2_train_labels.csv')
 #test_labels=pd.read_csv('/Users/Arthur/Desktop/ULB/2019-2020/Projet_Imagerie/rsna-pneumonia-detection-challenge/stage_2_test_labels.csv')
 
@@ -38,5 +42,36 @@ X_train, X_test, y_train, y_test = train_test_split(X,y, test_size = 0.2, random
 
 knn = KNeighborsClassifier(n_neighbors=6)
 knn.fit(X_train, y_train)
-
+"""
 """ Problem with the ID which is not a float... Other kinf of problem, no sens to do it here"""
+
+import keras
+from keras.layers import Dense
+from keras.models import Sequential
+
+""" PRoblem:  no module named keras
+tried: conda installl keras in terminall
+"""
+#################################################################################
+################# Keras type model construction #################################
+#################################################################################
+
+""" Typical case of a keras model prevision :
+    
+    We have a df: that we separate into predictors(n columns) and target (1 column)
+    
+    model=Sequential()  # Set up the model
+    
+    n_cols= predictors.shape[1]
+    
+    model.add(Dense(50, activation='relu', input_shape=(784,)))
+    model.add(Dense(50, activation='relu'))
+    model.add(Dense(10, activation='softmax'))
+    
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    
+    model.fit(X, y, validation_split=0.3)
+    
+    predictions = model.predict(pred_data)
+
+"""
